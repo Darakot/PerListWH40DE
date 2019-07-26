7601c3bf92fba6e4cd170698e92a10180d8137f1
Darakot <dmitriy0305@yandex.ru>
2019-07-26 17:29:41 +0500

    Start

---
+++ PerListWH40DE/src/module-info.java
@@ -0,0 +1,6 @@
+module PerListWH40DE {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens sample;
+}
---
+++ PerListWH40DE/src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import java.net.URL;
+        import java.util.ResourceBundle;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.CheckBox;
+        import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+
+public class Controller {
+
+}
