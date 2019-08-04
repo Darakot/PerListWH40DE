c0a6c9b23764283403eb15ca83f64a6e00a17c11
darakot <dmitriy0305@yandex.ru>
2019-08-04 20:23:44 +0500

    change name class

--- PerListWH40DE/src/sample/SqlClient.java
+++ PerListWH40DE/src/sample/SqlClient.java
@@ -6,7 +6,7 @@ import java.sql.*;
         private static Connection connection;
         private static Statement statement;
 
-        protected synchronized static void connect() {
+        public synchronized static void connect() {
             try {
                 Class.forName("org.sqlite.JDBC");
                 connection = DriverManager.getConnection("jdbc:sqlite:C:\\JavaApp\\PerListWH40DE\\src\\WH40K.db");
@@ -16,7 +16,7 @@ import java.sql.*;
             }
         }
 
-        protected synchronized static void disconnect() {
+        public synchronized static void disconnect() {
             try {
                 connection.close();
             } catch (SQLException e) {
@@ -24,8 +24,26 @@ import java.sql.*;
             }
         }
 
-        synchronized static String[] getThing (String thingName, String type) {
+       public synchronized static String[] getThing (String thingName, String type) {
             String request = "SELECT * FROM " + type + " WHERE nameAm='" + thingName + "'";
+            try (ResultSet set = statement.executeQuery(request)) {
+                if (set.next()) {
+                    ResultSetMetaData rsmd = set.getMetaData();
+                    String[] attributes = new String[rsmd.getColumnCount()];
+                    for (int i = 0; i<attributes.length; i++){
+                        attributes[i] = set.getString(i+1);
+                    }
+                    return attributes;
+                } else {
+                    return null;
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+       public synchronized static String[] getAttributes (String name, String type){
+            String request = "SELECT * FROM " + type + " WHERE nameAm='" + name + "'";
             try (ResultSet set = statement.executeQuery(request)) {
                 if (set.next()) {
                     ResultSetMetaData rsmd = set.getMetaData();
